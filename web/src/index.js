75c4f64b1bc12115db46be610557776ae18baea6
Rafael Barbosa <rafaelfbarbosa7@gmail.com>
2020-08-17 22:35:23 -0300

    Create a basic navigation bar

---
+++ web/src/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import './index.css';
+
+import Descontrolada from './containers/Descontrolada';
+
+ReactDOM.render(
+	<React.StrictMode>
+		<Descontrolada />
+	</React.StrictMode>,
+	document.getElementById('root')
+);
