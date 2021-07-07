5451f372ec31a3e6d536dfc66c6218d695a8982d
Rafael Barbosa <rafaelfbarbosa7@gmail.com>
2021-07-07 14:01:18 -0300

    Landing page finished

--- server/app.js
+++ server/app.js
@@ -22,7 +22,7 @@ app.use((req,res,next)=>{
     res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization')
     next()
 })
-
+    
 // Registering routes
 app.use('/auth', authRoutes)
 app.use('/user', userRoutes)
